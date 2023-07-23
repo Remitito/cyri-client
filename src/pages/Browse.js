fbc4e94411da7aa39c917c6caad847e52bc2bbe5
Remitito <JRossOT@protonmail.com>
2023-07-23 09:18:43 +0100

    Added extra message if it's taking a while to load

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -38,7 +38,6 @@ const Browse = () => {
             setState((prevState) => ({ ...prevState, retryCount: prevState.retryCount + 1 }));
           }
         }, 2000);
-    
         return () => clearTimeout(retryTimer);
       }, [state.currentTexts.length, state.retryCount]);
 
@@ -201,7 +200,13 @@ const Browse = () => {
             <>
             <Loading className='bebasNeue'>
             Loading... <LoadingSpinner/></Loading>
-            <LoadingMessage>If you are a first time user, it might take a moment to load.</LoadingMessage>
+            <>
+                {state.retryCount <= 3 ? <LoadingMessage>If you are a first time user, it might take a moment to load.</LoadingMessage>
+                :
+                <LoadingMessage>Almost there! Thanks for your patience.</LoadingMessage>
+                }
+            </>
+            
             </>}
         </>
     )}
